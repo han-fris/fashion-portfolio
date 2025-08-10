c7647e27eb25a78257d75f7b7a8e95549f1824f8
Han Frisonsova <hanafrisonsova@gmail.com>
2025-08-10 21:15:54 +0200

    change Header to mobile first, add Info and basic styling

--- src/pages/Info/Info.jsx
+++ src/pages/Info/Info.jsx
@@ -5,7 +5,8 @@ export const Info = () => {
   return (
     <PageWrapper>
       <div className="info__container container">
-        <h2>tady je něco o mě a kontakt</h2>
+        <h2>About me</h2>
+        <p>Fashion designer Hana Frišonsová is known for bold silhouettes, highly wearable clothing with experimental approach and using original prints. Her clothing is often variable, timeless and not defined by size / age / gender. She often collaborates with artists and other creative people. She presented her work at the FASHIONCLASH MAASTRICHT several times, she was awarded the ERSTE BANK FASHION AWARD at the Austria Fashion Awards 2011 in Vienna and she was nominated for the Czech Grand Design 2014. Hana Frišonsová is currently based in Prague</p>
       </div>
     </PageWrapper>
   );
